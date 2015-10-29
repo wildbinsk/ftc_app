dcf45124c1068cd6e47d88db05e362ebaf79f7bb
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2015-10-28 18:43:45 -0700

    Add an Encoder Test OpMode
    I have added a OpMode to test the encoder, other testing things will be
    added to this OpMode in the future.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -54,7 +54,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      *
      * If two or more op modes are registered with the same name, the app will display an error.
      */
-
+    manager.register("TestingTeleop", TestingTeleop.class);
     manager.register("NullOp", NullOp.class);
 
     manager.register("K9TeleOp", K9TeleOp.class);
