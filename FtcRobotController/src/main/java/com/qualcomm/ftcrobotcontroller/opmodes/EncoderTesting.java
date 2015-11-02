94e0bf25221749dcc02509205a9b29304386f54e
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2015-11-01 16:19:37 -0800

    Added More Encoder Stuff
    We now reset the actual *and* percieved motor positions.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderTesting.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderTesting.java
@@ -3,6 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.robocol.Telemetry;
 
 /**
  * Created by Declan Freeman-Gleason on 11/1/2015.
@@ -16,8 +17,16 @@ public class EncoderTesting extends OpMode {
         LeftMotor = hardwareMap.dcMotor.get("LeftMotor");
         LeftMotor.setDirection(DcMotor.Direction.REVERSE);
         RightMotor = hardwareMap.dcMotor.get("RightMotor");
+        telemetry.addData("Encoder Positions","LeftMotor: " + LeftMotor.getCurrentPosition() + " RightMotor: " + RightMotor.getCurrentPosition());
+        LeftMotor.setTargetPosition(0);
+        RightMotor.setTargetPosition(0);
+        telemetry.addData("Encoder Positions", "LeftMotor: " + LeftMotor.getCurrentPosition() + " RightMotor: " + RightMotor.getCurrentPosition());
         LeftMotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
         RightMotor.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
+        telemetry.addData("Encoder Positions", "LeftMotor: " + LeftMotor.getCurrentPosition() + " RightMotor: " + RightMotor.getCurrentPosition());
+    }
+    @Override
+    public void loop() {
         // Encoders have been reset.
         LeftMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
         RightMotor.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
@@ -25,15 +34,13 @@ public class EncoderTesting extends OpMode {
         RightMotor.setTargetPosition(PPM);
         LeftMotor.setPower(.25);
         RightMotor.setPower(.25);
-    }
-    @Override
-    public void loop() {
+        telemetry.addData("Encoder Positions","LeftMotor: " + LeftMotor.getCurrentPosition() + " RightMotor: " + RightMotor.getCurrentPosition());
 //        if ((LeftMotor.getTargetPosition() - LeftMotor.getCurrentPosition()) < PPM) {
 //            LeftMotor.setPowerFloat();
 //        }
 //        if ((RightMotor.getTargetPosition() - RightMotor.getCurrentPosition()) < PPM) {
 //            RightMotor.setPowerFloat();
 //        }
-        
+
     }
 }
